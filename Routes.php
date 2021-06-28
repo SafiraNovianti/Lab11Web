f0dccb2e4a9b6503ad09db5ae3ca9fc87cc4efd9
SafiraNovianti <56381081+SafiraNovianti@users.noreply.github.com>
2021-06-28 22:48:04 +0700

    Add files via upload

--- Routes.php
+++ Routes.php
@@ -39,12 +39,13 @@ $routes->get('/faqs', 'page::faqs');
 
 $routes->get('/artikel/(:any)', 'Artikel::view/$1');
 
-$routes->group('admin', function($routes) {
+$routes->group('admin', ['filters' => 'auth'], function($routes) 
+{
 	$routes->get('artikel', 'Artikel::admin_index');
 	$routes->add('artikel/add', 'Artikel::add');
 	$routes->add('artikel/edit/(:any)', 'Artikel::edit/$1');
 	$routes->get('artikel/delete/(:any)', 'Artikel::delete/$1');
-	});
+});
 /*
  * --------------------------------------------------------------------
  * Additional Routing
